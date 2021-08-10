0a0029638eba3f2c61701279669ed4d3445c9271
Paramvir Singh <ps17cw@brocku.ca>
2021-08-10 13:48:11 -0400

    proj struct changed

---
+++ build/CMakeFiles/Spell-Checker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Spell-Checker.dir/src/main.cpp.o"
+  "Spell-Checker"
+  "Spell-Checker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Spell-Checker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
